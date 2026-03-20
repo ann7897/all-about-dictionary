9f8ed05da64cd34ca855145fa78883e7d0a36f4a
ann7897 <hubertprakash@hotmail.com>
2026-03-20 19:15:37 +0000

    Add files via upload

---
+++ activety2.py
@@ -0,0 +1,12 @@
+
+country_code = {'India' : '0091',
+                'Australia' : '0025',
+                'Nepal' : '00977'}
+
+
+print("country code for India -")
+print(country_code.get('India', 'not found'))
+
+
+print("country code for Japen -")
+print(country_code.get('Japan', 'Not found'))
